01c23d8ed90804a7025a9f939cf3d68fa9072a28
Felix Seidel <felix@seidel.me>
2023-04-29 16:00:36 +0700

    ♻️ Add tests

---
+++ test/_global/all-tools.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+# Feature-specific tests
+# The 'check' command comes from the dev-container-features-test-lib.
+check "asdf list act" asdf list act
+check "k9s" k9s version
+check "k3d" k3d version
+check "kubectx" kubectx --help
+check "kubens" kubens --help
+
+# Report result
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
